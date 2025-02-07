43446ce0dff318d54d3587d481bf59a9cde08057
Pavel <voroshilow_pavel@mail.ru>
2025-02-07 18:27:00 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+//https://github.com/stemmlerjs/simple-typescript-starter/tree/master
+import {test} from './test'
+console.log(test(100,24));
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+export function test (a:number, b:number): number {
+    return a+b;
+}
+
+export default test
